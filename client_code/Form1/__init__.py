5fc0fa0fab15abf774ce26aa0a9503e8ae6d904e
justin.herb91@gmail.com <justin.herb91@gmail.com>
2024-12-23 01:07:37 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,10 +1,10 @@
 from ._anvil_designer import Form1Template
 from anvil import *
-# Change the import to use relative path
-from . import MonitoringControl
 
 class Form1(Form1Template):
     def __init__(self, **properties):
         self.init_components(**properties)
+        # Import MonitoringControl here to avoid circular imports
+        from .MonitoringControl import MonitoringControl
         # Add the MonitoringControl to the content_panel
-        self.content_panel.add_component(MonitoringControl.MonitoringControl())
+        self.content_panel.add_component(MonitoringControl())
